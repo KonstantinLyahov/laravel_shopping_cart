f409ea01d464c1471ae9f479950528fb6f911b37
Constantinely <43109874+Constantinely@users.noreply.github.com>
2020-12-06 23:02:37 +0200

    product index view

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,11 +1,28 @@
 @extends('layouts.master')
 
 @section('title')
-	 Laravel shopping cart
+Laravel shopping cart
 @endsection
 
 @section('content')
-	<div class="container">
-		<h1>It works!</h1>
-	</div>
+<div class="row">
+	@for ($i = 0; $i < 6; $i++)	 
+		<div class="col-sm-6 col-md-4 mt-2">
+			<div class="card" style="width: 18rem;">
+				<div class="card-body">
+					<img src="https://chubarov.if.ua/images/book_design_2.jpg?crc=502489758" class="card-img-top"
+						alt="...">
+					<h5 class="card-title">Product title</h5>
+					<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem architecto ad sit laborum
+						quo atque. Blanditiis consequatur laudantium, iusto hic, qui doloremque vero corrupti cum suscipit deserunt
+						repellendus dolorum eveniet.</p>
+						<div class="d-flex justify-content-between">
+							<div class="price">12$</div>
+							<a href="#" class="btn btn-success">Add to cart</a>
+						</div>
+				</div>
+			</div>
+		</div>
+	@endfor
+</div>
 @endsection
