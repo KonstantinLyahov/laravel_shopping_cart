c0a701ef2523086ebfe4f0a16b64b6b40c45250d
Constantinely <43109874+Constantinely@users.noreply.github.com>
2020-12-06 23:53:05 +0200

    outputting product data

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function getIndexPage()
+    {
+        $products = Product::paginate(6);
+        return view('shop/index', ['products' => $products]);
+    }
+}
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -6,23 +6,24 @@ Laravel shopping cart
 
 @section('content')
 <div class="row">
-	@for ($i = 0; $i < 6; $i++)	 
+	@foreach ($products as $product)	 
 		<div class="col-sm-6 col-md-4 mt-2">
 			<div class="card" style="width: 18rem;">
 				<div class="card-body">
-					<img src="https://chubarov.if.ua/images/book_design_2.jpg?crc=502489758" class="card-img-top"
+					<img src="{{ $product->image_path }}" class="card-img-top"
 						alt="...">
-					<h5 class="card-title">Product title</h5>
-					<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem architecto ad sit laborum
-						quo atque. Blanditiis consequatur laudantium, iusto hic, qui doloremque vero corrupti cum suscipit deserunt
-						repellendus dolorum eveniet.</p>
+					<h5 class="card-title">{{ $product->title }}</h5>
+					<p class="card-text">{{ $product->description }}</p>
 						<div class="d-flex justify-content-between">
-							<div class="price">12$</div>
+							<div class="price">{{ $product->price }}$</div>
 							<a href="#" class="btn btn-success">Add to cart</a>
 						</div>
 				</div>
 			</div>
 		</div>
-	@endfor
+	@endforeach
+</div>
+<div class="mt-2 text-center">
+	{{ $products->links() }}
 </div>
 @endsection
